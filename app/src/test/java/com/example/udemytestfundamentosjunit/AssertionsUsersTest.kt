7f939142c00cde2063e6c2c5315a612c11f6b787
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-06-06 16:32:09 +0200

    test: Section 2-19
    - set @BeforeClass & @AfterClass

--- app/src/test/java/com/example/udemytestfundamentosjunit/AssertionsUsersTest.kt
+++ app/src/test/java/com/example/udemytestfundamentosjunit/AssertionsUsersTest.kt
@@ -1,22 +1,42 @@
 package com.example.udemytestfundamentosjunit
 
 import org.junit.After
-import org.junit.Assert.*
+import org.junit.AfterClass
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNotSame
+import org.junit.Assert.assertSame
+import org.junit.Assert.assertTrue
 import org.junit.Before
-
+import org.junit.BeforeClass
 import org.junit.Test
 
 class AssertionsUsersTest {
-
-    private lateinit var assertions : Assertions
     private lateinit var bot : User
-    private lateinit var human : User
+
+    companion object {
+        private lateinit var assertions : Assertions
+        private lateinit var human : User
+
+        @BeforeClass @JvmStatic
+        fun setupCommon() {
+            assertions = Assertions()
+            human = User("Ruben", 25, true)
+            println("BEFORE_CLASS")
+        }
+
+        @AfterClass @JvmStatic
+        fun tearDownCommon() {
+            assertions = Assertions()
+            human = User()
+            println("AFTER_CLASS")
+        }
+    }
 
     @Before
     fun setup() {
         assertions = Assertions()
         bot = User("Robot", 1, false)
-        human = User("Ruben", 25, true)
         println("BEFORE")
     }
 
@@ -24,7 +44,6 @@ class AssertionsUsersTest {
     fun tearDown() {
         assertions = Assertions()
         bot = User()
-        human = User()
         println("AFTER")
     }
 
@@ -39,5 +58,19 @@ class AssertionsUsersTest {
     fun checkNotNullUserTest() {
         assertNotNull(assertions.checkHuman(human))
         println("test: checkNotNullUserTest()")
+    }
+
+    @Test
+    fun checkNotSameUserTest(){
+        assertNotSame(bot, human)
+        println("test: checkNotSameUserTest()")
+    }
+
+    @Test
+    fun checkSameUserTest(){
+        val humanCopy = human
+
+        assertSame(humanCopy, human)
+        println("test: checkSameUserTest()")
     }
 }
