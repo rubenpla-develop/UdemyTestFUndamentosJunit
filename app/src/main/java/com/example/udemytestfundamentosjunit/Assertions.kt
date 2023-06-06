e2c38b0ede83e093356952cb4d6a493c2c1641de
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-06-06 15:50:31 +0200

    feat: create Assertions & User classes

---
+++ app/src/main/java/com/example/udemytestfundamentosjunit/Assertions.kt
@@ -0,0 +1,28 @@
+package com.example.udemytestfundamentosjunit
+
+class Assertions {
+    private val user = User("Ruben", 41)
+
+    fun getLuckyNumbers() : Array<Int> {
+        return arrayOf(21, 117)
+    }
+
+    fun getName() : String {
+        return user.name
+    }
+
+    fun checkHuman(user: User) : Boolean {
+        return user.isHuman
+    }
+
+    fun checkHuman(user: User? = null) : Boolean? {
+        if  (user == null) return null
+        return user.isHuman
+    }
+
+    fun isAdult(user : User) : Boolean {
+        if (!user.isHuman) return true
+
+        return user.age >= 18
+    }
+}
---
+++ app/src/main/java/com/example/udemytestfundamentosjunit/User.kt
@@ -0,0 +1,5 @@
+package com.example.udemytestfundamentosjunit
+
+class User (val name: String = "",
+            val age: Int = 0,
+            val isHuman: Boolean = true)
